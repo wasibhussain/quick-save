2a56d436ca6f6c447dbdd71ef94a509500e08c30
Wasib Hussain <wasibhussain3@gmail.com>
2025-06-11 15:59:20 +0000

    great ,the theme should be light not dark please modify and add some pad

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
       </head>
-      <body className="dark font-body antialiased">
+      <body className="font-body antialiased"> {/* Removed 'dark' class */}
         {children}
         <Toaster />
       </body>
